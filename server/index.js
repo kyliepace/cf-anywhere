1c3eae27d69106f2dacc4e7e68803893e323c520
Kylie Pace <kyliepace@gmail.com>
2018-10-16 13:03:02 -0700

    removed redis since we are using jwt

--- server/index.js
+++ server/index.js
@@ -4,19 +4,14 @@ const koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const router = require('./routes.js');
 const app = new koa();
-const session = require('./config/session-options.js');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 
 // DB Setup
-mongoose.connect('mongodb://localhost/auth');
+//mongoose.connect('mongodb://localhost/auth');
 
 app.use(cors());
-
-//session management in redis
-app.keys = [process.env.KOA_SESSION_SECRET];
-app.use(session(app));
 
 // body parser
 app.use(bodyParser());
